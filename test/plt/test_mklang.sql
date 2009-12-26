21d04cd2f240fb8d8be20641c4b8917ff1e6ffd6
Akinori MUSHA <knu@idaemons.org>
2009-12-27 02:41:14 +0900

    Remove auto-generated files.

--- test/plt/test_mklang.sql
+++
@@ -1,8 +0,0 @@
-
-   create function plruby_call_handler() returns language_handler
-   as '/home/ts/ruby/devel/plruby-0.5.2/src/plruby.so'
-   language 'C';
-
-   create trusted  language 'plruby'
-	handler plruby_call_handler
-	lancompiler 'PL/Ruby';
